b6e406f2441be7f98ec2efa8cbb468712a05e011
Tomasz Klim <github@tomaszklim.pl>
2018-06-15 18:23:19 +0200

    first version

---
+++ README.md
@@ -0,0 +1,2 @@
+sf-log-manager is a proxy extension, responsible for detecting
+server state and installing adequate set of extensions.
---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+. /opt/farm/scripts/init
+
+
+if [ ! -d /opt/farm/ext/repos/lists/$OSVER ] || [ "$OSTYPE" = "suse" ] || [ "$OSTYPE" = "qnap" ]; then
+	echo "skipping syslog configuration, unsupported system version"
+elif [ "$SYSLOG" != "true" ]; then
+	/opt/farm/scripts/setup/extension.sh sf-log-forwarder
+else
+	/opt/farm/scripts/setup/extension.sh sf-log-receiver
+	/opt/farm/scripts/setup/extension.sh sf-log-monitor
+fi
+
+/opt/farm/scripts/setup/extension.sh sf-log-rotate
